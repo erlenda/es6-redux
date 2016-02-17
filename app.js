a57c6763ffbe8e7de65f3216ec08ee947e2a34e9
Erlend Andresen <erlend.andresen@gmail.com>
2016-02-17 22:57:40 +0100

    added gulp redux reqs

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var _expect = require('expect');
+
+var _expect2 = _interopRequireDefault(_expect);
+
+var _redux = require('redux');
+
+var _redux2 = _interopRequireDefault(_redux);
+
+var _deepFreeze = require('deep-freeze');
+
+var _deepFreeze2 = _interopRequireDefault(_deepFreeze);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// redux
+// TODO: Start with egghead.io
+
+// tests
+(0, _expect2.default)("s").toEqual("s");
+console.log("all tests OK");
+//# sourceMappingURL=app.js.map
---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import expect from 'expect'
+import redux from 'redux'
+import deepfreeze from 'deep-freeze'
+
+// redux
+// TODO: Start with egghead.io 
+
+
+// tests
+expect("s").toEqual("s");
+console.log("all tests OK")
